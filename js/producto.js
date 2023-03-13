72030a170e24607a87a6687b013818e8d9f12113
Miguel Rondanelli <miguel.rondanelli.inf@gmail.com>
2023-03-13 11:26:20 -0300

    rama master, primer commit

---
+++ js/producto.js
@@ -0,0 +1,20 @@
+//Variables
+
+const valor = document.querySelector('#valor');
+const rango = document.querySelector('.form-range');
+console.log(valor);
+
+eventListener();
+
+//Eventos
+
+function eventListener(){
+
+    rango.addEventListener('change', ()=>{
+
+        valor.value = rango.value;
+
+    });
+
+}
+
